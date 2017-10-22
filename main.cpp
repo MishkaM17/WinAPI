cd0f4a15d54389e379e34860242de5ddb0ade563
MishkaM17 <32699613+MishkaM17@users.noreply.github.com>
2017-10-22 14:50:29 +0200

    0.0.3

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <windows.h>
+#include <cstdlib>
+#include <ctime>
+
+
 
 #define ID_PRZYCISK1 501
 #define ID_PRZYCISK2 502
-#define ID_PRZYCISK3 503
 
 LPSTR NazwaKlasy = "Klasa Okienka";
 MSG Komunikat;
 HWND g_hDalej;
-HWND g_hWiecej;
-HWND g_hMniej;
+HWND g_hWyjdz;
 HWND hStatic;
 
 LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );
+
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -35,18 +38,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     RegisterClassEx( & wc );
     
 	HWND hwnd;
-	hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, NazwaKlasy, "Zgadywanka", WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, NULL, NULL, hInstance, NULL );
+	hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, NazwaKlasy, "Zgadywanka", WS_CAPTION,
+    CW_USEDEFAULT, CW_USEDEFAULT, 310, 300, NULL, NULL, hInstance, NULL );
     
    		hStatic = CreateWindowEx (0, "STATIC", NULL, WS_CHILD | WS_VISIBLE | SS_LEFT, 2, 2 ,300, 80, hwnd, NULL, hInstance, NULL);
-			SetWindowText(hStatic, "Pomyœl o liczbê z przedzia³u od 0 do 30!");
+			SetWindowText(hStatic, "Pomyœl o liczbê z przedzia³u od 0 do 30, a nastêpnie naciœnij Dalej!");
    		
-		g_hDalej = CreateWindowEx( 0, "BUTTON", "Dalej!", WS_CHILD | WS_VISIBLE, 110, 150, 50, 30, hwnd, ( HMENU ) ID_PRZYCISK1, hInstance, NULL );
-   		g_hWiecej = CreateWindowEx( 0, "BUTTON", "Wiêcej!", WS_CHILD | WS_VISIBLE, 160, 180, 60, 30, hwnd, ( HMENU ) ID_PRZYCISK2, hInstance, NULL );
-   		g_hMniej = CreateWindowEx( 0, "BUTTON", "Mniej", WS_CHILD | WS_VISIBLE, 60, 180, 50, 30, hwnd, ( HMENU ) ID_PRZYCISK3, hInstance, NULL );
+   		g_hDalej = CreateWindowEx( 0, "BUTTON", "Dalej!", WS_CHILD | WS_VISIBLE, 30, 180, 80, 30, hwnd, ( HMENU ) ID_PRZYCISK1, hInstance, NULL );
+   		g_hWyjdz = CreateWindowEx( 0, "BUTTON", "WyjdŸ!", WS_CHILD | WS_VISIBLE, 160, 180, 80, 30, hwnd, ( HMENU ) ID_PRZYCISK2, hInstance, NULL );
 		
 		
-	
 
     
 	ShowWindow( hwnd, nCmdShow );
@@ -76,6 +77,39 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         
         default:
         return DefWindowProc( hwnd, msg, wParam, lParam );
+        
+    case WM_COMMAND:
+		switch( wParam )
+	{
+		case ID_PRZYCISK1:
+    	MessageBox( hwnd, "Czy Twoja liczba jest wiêksza od 15?", "Zgadywanka", MB_YESNO );
+    		case IDYES:
+    			
+				srand( (unsigned)time( NULL ) ); 
+      			char l[2];
+      			l[0] = (rand()%15)+48;
+     			l[1] = NULL;
+      			MessageBox (NULL, l, "Zgdywanka", MB_YESNO );
+      			break;
+					
+				   	
+    		
+			case IDNO:
+    			srand( (unsigned)time( NULL ) ); 
+      			char j[2];
+      			j[0] = (rand()%6)+48;
+      			j[1] = NULL;
+      			MessageBox (NULL, j, "Zgadywanka", MB_YESNO);
+    
+    break;
+    
+		case ID_PRZYCISK2:
+    	MessageBox( hwnd, "Dziêkujemy za skorzystanie ze zgadywanki, do zobaczenia!", "Zgadywanka", MB_ICONINFORMATION );
+   	 	 exit(0);
+    
+    	
+	}
+	break;
     }
     
     return 0;
