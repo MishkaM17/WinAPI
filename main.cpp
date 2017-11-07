631cd0b049d5ef33202f5ecff54bd23d08cd96e5
MishkaM17 <32699613+MishkaM17@users.noreply.github.com>
2017-11-07 14:45:06 +0100

    0.0.4

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include <windows.h>
 #include <cstdlib>
-#include <ctime>
-
 
 
 #define ID_PRZYCISK1 501
 #define ID_PRZYCISK2 502
+#define ID_PRZYCISK3 503
+#define ID_PRZYCISK4 504
+#define ID_PRZYCISK5 505
+
 
 LPSTR NazwaKlasy = "Klasa Okienka";
 MSG Komunikat;
 HWND g_hDalej;
 HWND g_hWyjdz;
+HWND g_hPrzedzial1;
+HWND g_hPrzedzial2;
+HWND g_hPrzedzial3;
 HWND hStatic;
 
 LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );
-
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -39,17 +43,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     
 	HWND hwnd;
 	hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, NazwaKlasy, "Zgadywanka", WS_CAPTION,
-    CW_USEDEFAULT, CW_USEDEFAULT, 310, 300, NULL, NULL, hInstance, NULL );
+    CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, NULL, NULL, hInstance, NULL );
     
-   		hStatic = CreateWindowEx (0, "STATIC", NULL, WS_CHILD | WS_VISIBLE | SS_LEFT, 2, 2 ,300, 80, hwnd, NULL, hInstance, NULL);
-			SetWindowText(hStatic, "Pomyœl o liczbê z przedzia³u od 0 do 30, a nastêpnie naciœnij Dalej!");
+   		hStatic = CreateWindowEx (0, "STATIC", NULL, WS_CHILD | WS_VISIBLE | SS_LEFT, 2, 2 ,400, 80, hwnd, NULL, hInstance, NULL);
+			SetWindowText(hStatic, "Pomyœl o liczbê z przedzia³u od 1 do 15, a nastêpnie naciœnij Dalej!");
    		
-   		g_hDalej = CreateWindowEx( 0, "BUTTON", "Dalej!", WS_CHILD | WS_VISIBLE, 30, 180, 80, 30, hwnd, ( HMENU ) ID_PRZYCISK1, hInstance, NULL );
-   		g_hWyjdz = CreateWindowEx( 0, "BUTTON", "WyjdŸ!", WS_CHILD | WS_VISIBLE, 160, 180, 80, 30, hwnd, ( HMENU ) ID_PRZYCISK2, hInstance, NULL );
-		
-		
-
-    
+   		g_hDalej = CreateWindowEx( 0, "BUTTON", "Dalej!", WS_CHILD | WS_VISIBLE, 80, 180, 90, 50, hwnd, ( HMENU ) ID_PRZYCISK1, hInstance, NULL );
+   		g_hWyjdz = CreateWindowEx( 0, "BUTTON", "WyjdŸ!", WS_CHILD | WS_VISIBLE, 200, 180, 90, 50, hwnd, ( HMENU ) ID_PRZYCISK2, hInstance, NULL );
+   		g_hPrzedzial1 = CreateWindowEx( 0, "BUTTON", "Przedzia³ nr 1", WS_CHILD | WS_VISIBLE, 10, 300, 110, 30, hwnd, ( HMENU ) ID_PRZYCISK3, hInstance, NULL );
+   		g_hPrzedzial2 = CreateWindowEx( 0, "BUTTON", "Przedzia³ nr 2", WS_CHILD | WS_VISIBLE, 140, 300, 110, 30, hwnd, ( HMENU ) ID_PRZYCISK4, hInstance, NULL );
+   		g_hPrzedzial3 = CreateWindowEx( 0, "BUTTON", "Przedzia³ nr 3", WS_CHILD | WS_VISIBLE, 270, 300, 110, 30, hwnd, ( HMENU ) ID_PRZYCISK5, hInstance, NULL );
+ 		
 	ShowWindow( hwnd, nCmdShow );
     UpdateWindow( hwnd );
     
@@ -67,6 +71,101 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
      switch( msg )
     {
+    
+    	case WM_COMMAND:
+    	switch( wParam )
+   	{
+    	case ID_PRZYCISK1: 
+    		MessageBox( hwnd, "Wybierz przedzia³, do którego nale¿y Twoja liczba:\n Nr 1: <1, 5>\nNr 2: <6, 10>\nNr 3: <11, 15>", "Zgadywanka", MB_ICONQUESTION);
+			return(0);	
+		case ID_PRZYCISK3:
+			{
+			int a= MessageBox( hwnd, "Czy Twoja liczba jest parzysta?", "Zgadywanka", MB_YESNO | MB_ICONQUESTION);
+			if(a==IDYES)
+			{
+			int b = MessageBox( hwnd, "Twoja liczba to 4!", "Zgadywanka", MB_RETRYCANCEL | MB_ICONQUESTION);
+			if(b==IDRETRY)
+			MessageBox( hwnd, "Twoja liczba to 2!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+			return(0);
+			}
+			if(a==IDNO)
+			{
+			int c = MessageBox( hwnd, "Twoja liczba to 3!", "Zgadywanka", MB_RETRYCANCEL | MB_ICONQUESTION);
+			if (c==IDRETRY)
+			{
+			int f = MessageBox( hwnd, "Twoja liczba to 5!", "Zgadywanka", MB_RETRYCANCEL| MB_ICONQUESTION);
+			if(f==IDRETRY)
+			MessageBox( hwnd, "Twoja liczba to 1!", "Zgadywanka", MB_OK| MB_ICONQUESTION);}}
+			return(0);
+			}
+		case ID_PRZYCISK4:
+			{
+				{
+				int l = MessageBox( hwnd, "Czy Twoja liczba to któraœ z tych liczb:\n 6,7,8", "Zgadywanka", MB_YESNO | MB_ICONQUESTION);
+					if(l==IDYES)
+					{
+						int k = MessageBox( hwnd, "Czy Twoja liczba jest parzysta?", "Zgadywanka", MB_YESNO | MB_ICONQUESTION);
+						if(k==IDYES)
+							{
+							int j=MessageBox( hwnd, "Twoja liczba to 6!", "Zgadywanka", MB_RETRYCANCEL | MB_ICONQUESTION);
+							if(j==IDRETRY)
+							MessageBox( hwnd, "Twoja liczba to 8!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+							return(0);
+							}
+						if(k==IDNO)
+							MessageBox( hwnd, "Twoja liczba to 7!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+							return(0);
+					}
+					if(l==IDNO)
+					{
+						int h = MessageBox( hwnd, "Czy Twoja liczba jest nieparzysta?", "Zgadywanka", MB_YESNO | MB_ICONQUESTION);
+						if(h==IDYES)
+							MessageBox( hwnd, "Twoja liczba to 9!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+							return(0);
+						if(h==IDNO)
+							MessageBox( hwnd, "Twoja liczba to 10!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+							return(0);
+					}
+				}
+			}
+		case ID_PRZYCISK5:
+			{
+				{
+				int g = MessageBox( hwnd, "Czy Twoja liczba jest nieparzysta?", "Zgadywanka", MB_YESNO | MB_ICONQUESTION);
+					if(g==IDNO)
+					{
+					int f = MessageBox( hwnd, "Twoja liczba to 12!", "Zgadywanka", MB_RETRYCANCEL | MB_ICONQUESTION);
+					if(f==IDRETRY)
+					MessageBox( hwnd, "Twoja liczba to 14!", "Zgadywanka", MB_RETRYCANCEL | MB_ICONQUESTION);
+					return(0);
+					}
+					if(g==IDYES)
+					{
+					int p = MessageBox( hwnd, "Czy Twoja liczba jest podzielna przez 3?", "Zgadywanka", MB_YESNO | MB_ICONQUESTION);
+					if(p==IDYES)
+					MessageBox( hwnd, "Twoja liczba to 15!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+					if(p==IDNO)
+					{
+					int o= MessageBox( hwnd, "Twoja liczba to 11", "Zgadywanka", MB_RETRYCANCEL | MB_ICONQUESTION);
+					if(o==IDRETRY)
+					MessageBox( hwnd, "Twoja liczba to 13!", "Zgadywanka", MB_OK | MB_ICONQUESTION);
+					return(0);
+					}
+					}	
+				}
+			}	
+    		
+	
+		case ID_PRZYCISK2:
+    	MessageBox( hwnd, "Dziêkujemy za skorzystanie ze zgadywanki, do zobaczenia!", "Zgadywanka", MB_ICONINFORMATION );
+   	 	 exit(0);
+	
+	
+	}
+	
+	
+	
+    	
     case WM_CLOSE:
         DestroyWindow( hwnd );
         break;
@@ -77,40 +176,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         
         default:
         return DefWindowProc( hwnd, msg, wParam, lParam );
-        
-    case WM_COMMAND:
-		switch( wParam )
-	{
-		case ID_PRZYCISK1:
-    	MessageBox( hwnd, "Czy Twoja liczba jest wiêksza od 15?", "Zgadywanka", MB_YESNO );
-    		case IDYES:
-    			
-				srand( (unsigned)time( NULL ) ); 
-      			char l[2];
-      			l[0] = (rand()%15)+48;
-     			l[1] = NULL;
-      			MessageBox (NULL, l, "Zgdywanka", MB_YESNO );
-      			break;
-					
-				   	
-    		
-			case IDNO:
-    			srand( (unsigned)time( NULL ) ); 
-      			char j[2];
-      			j[0] = (rand()%6)+48;
-      			j[1] = NULL;
-      			MessageBox (NULL, j, "Zgadywanka", MB_YESNO);
-    
-    break;
-    
-		case ID_PRZYCISK2:
-    	MessageBox( hwnd, "Dziêkujemy za skorzystanie ze zgadywanki, do zobaczenia!", "Zgadywanka", MB_ICONINFORMATION );
-   	 	 exit(0);
-    
-    	
-	}
-	break;
     }
+        
+    
+	
     
     return 0;
 }
+
+
+
